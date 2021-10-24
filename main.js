66df2f6c3d99793165f7e9fe96f78ab20f2325c5
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2021-10-24 14:13:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+Webcam.set({
+
+width : 350,
+height : 300,
+image_format : 'png',
+png_quality :90
+
+});
+
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+
+function capture_image(){
+
+Webcam.snap(function(data_uri){
+
+document.getElementById("result").innerHTML = "<img id='captured_image' src="+data_uri+">";
+
+})
+
+}
+
+console.log('ml5 version is', ml5.version);
+
+
+
+var app_mdle = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/4lSDW9ci7/model.json",modelLoaded);
